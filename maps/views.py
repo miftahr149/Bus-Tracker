7698f496c4b5f099a42b7ded3089e57a7225ca77
miftahr149 <miftahr149@gmail.com>
2023-11-06 11:38:10 +0800

    Adding Common Folder
    Common Folder is used to store file that can be used in any apps

--- maps/views.py
+++ maps/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from common import views as common_views
 
 # Create your views here.
 def get_map(request: HttpRequest) -> HttpResponse:
-    return render(request, 'maps.html')
+    header, footer = common_views.get_header_footer(request)
+    body = render(request, 'maps.html')
+    full_html = header + body + footer
+    return full_html
