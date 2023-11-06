463d458e9e2813e50367a09e74db836ae4ee7dfa
miftahr149 <miftahr149@gmail.com>
2023-11-06 18:31:05 +0800

    Changing COMMON_TEMPLATES_PATH Variable

--- common/utility.py
+++ common/utility.py
@@ -3,11 +3,11 @@ from django.http import HttpRequest, HttpResponse
 import os
 from pathlib import Path
 
-COMMON_TEMPLATES_PATH = 'C://Users//mifta//OneDrive//Documents//Github//Bus-Tracker//common//templates'
+COMMON_TEMPLATES_PATH = Path().absolute().joinpath('common', 'templates')
 
 def get_header_footer(request: HttpRequest) -> tuple[HttpResponse, HttpResponse]:
-    header = render(request, f'{COMMON_TEMPLATES_PATH}/header.html')
-    footer = render(request, f'{COMMON_TEMPLATES_PATH}/footer.html')
+    header = render(request, get_template_path('header.html'))
+    footer = render(request, get_template_path('footer.html'))
     return header, footer
 
 def combine_content(*pages: HttpResponse) -> HttpResponse:
@@ -15,3 +15,6 @@ def combine_content(*pages: HttpResponse) -> HttpResponse:
     for page in pages[1:]:
         result = result + page.content
     return result
+
+def get_template_path(pathstr: str) -> Path:
+    return COMMON_TEMPLATES_PATH.joinpath(pathstr)
